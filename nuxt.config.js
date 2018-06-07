ec40085eeb37b21ddfae238a8c49006c2403c49f
Gardem <dmitryarchebasov@gmail.com>
2018-06-07 13:14:33 +0300

    nuxt.config update

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,31 +3,25 @@ module.exports = {
     ['bootstrap-vue/nuxt']
   ],
 
-  /*
-  ** Headers of the page
-  */
   head: {
-    title: 'demo-carousel',
+    title: 'Vue bootstrap carousel demo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Vue bootstrap carousel demo ssr' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress bar color
-  */
+
   loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
+
   build: {
     vendor: [
       'babel-polyfill'
     ],
+
     /*
     ** Run ESLint on save
     */
